119304a9f0f7997616a2743236387534acbfcd6d
Leonel Cassul <leonelsalmos@gmail.com>
2023-08-06 11:34:49 +0100

    Fundamentos_1

---
+++ fundamentos.js
@@ -0,0 +1,33 @@
+// Introdução as Váriaveis
+// Sintaxe: var identificador = "valor";
+var minhaVariavel = "Olá, Mundo";
+
+console.log(minhaVariavel);
+console.log(typeof minhaVariavel);
+
+// let e const :  são as formas que a linguagem usapara a declaração das váriaveis.
+
+let x =  10;
+const y = 5;
+
+console.log(x)
+console.log(y);
+
+// Operadores Aritmético
+
+console.log("Operadores Aritmetricos");
+console.log("Opreção com ", x," e",y)
+
+console.log("Soma ou Adição: ", x + y);
+console.log("Subtração: ", x - y);
+console.log("Multiplicação: ", x * y);
+console.log(" Divisão: ", x / y);
+
+// Operadores de Comparação
+
+console.log(x == y);
+console.log( x === y);
+console.log(x!=y);
+console.log(x !== y);
+
+
