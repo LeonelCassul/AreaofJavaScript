30d0f627149d0bcdd9f20f1136b0fa4e4ef8a1ad
Leonel Cassul <leonelsalmos@gmail.com>
2023-08-14 23:53:43 +0100

    Arraow function

---
+++ teoria/javascript_moderno.js
@@ -0,0 +1,12 @@
+// Arrow function
+
+const   soma = (a, b) => a + b;
+console.log(soma(3, 5));
+
+const areaTriangulo = (a , b) =>{
+    const area = ((a + a) *(b+b))/2;
+    return area;
+}
+
+console.log(areaTriangulo(6,7));
+   
